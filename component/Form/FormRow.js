986e78b21e9c195383e66dc6b286004eb78fb23e
Falaleev Maxim <max@studio107.ru>
2018-11-15 17:17:50 +0300

    CL-232

--- component/Form/FormRow.js
+++ component/Form/FormRow.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
+import { bem } from '..';
 
 export class FormRow extends Component {
   static propTypes = {
     className: PropTypes.string,
-    children: PropTypes.any.isRequired
+    children: PropTypes.any.isRequired,
+    modifiers: PropTypes.any
   };
 
   static defaultProps = {
-    className: ''
+    modifiers: [],
+    className: null
   };
 
   render() {
     const {
+      modifiers,
       className,
       children,
       ...rest
     } = this.props;
 
     return (
-      <div className={cx('b-form__row', className)} {...rest}>
+      <div className={cx(bem('b-form__row', modifiers), className)} {...rest}>
         {children}
       </div>
     );
